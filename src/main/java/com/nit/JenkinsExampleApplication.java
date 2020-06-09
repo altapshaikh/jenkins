cf2d1c159582e0a5f344526e0e11cbaf3fef1009
altapshaikh <altap_shaikh111@rediffmail.com>
2020-06-09 15:32:25 +0530

    first commit

---
+++ src/main/java/com/nit/JenkinsExampleApplication.java
@@ -0,0 +1,21 @@
+package com.nit;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsExampleApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsExampleApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("===================================================");
+		System.out.println("*********   welcome to jenkins     **************");
+		System.out.println("===================================================");
+	}
+
+}
